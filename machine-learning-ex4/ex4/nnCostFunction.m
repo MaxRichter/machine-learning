828f3ea7497fffaee3b043fe8ae1040741232ecf
maxrichter-scdm <max.richter@scdm.de>
2017-03-01 11:59:05 +0100

    Added ex5

--- machine-learning-ex4/ex4/nnCostFunction.m
+++ machine-learning-ex4/ex4/nnCostFunction.m
@@ -62,23 +62,118 @@ Theta2_grad = zeros(size(Theta2));
 %               and Theta2_grad from Part 2.
 %
 
+% 3-layer neural network
+% input_layer_size = 400
+% hidden_layer_size = 25
+% num_labels = 10
+% m = 5000
+% X = 5000x400
+% y = 5000x1
+% lambda = 0
+% Theta1 = 25x401
+% Theta2 = 10x26
+
+% Add ones to the first column of data matrix X
+a1 = [ones(m, 1) X]; % 5000x401
+% Calculate the hidden layer
+z2 = Theta1 * a1'; % 25x5000
+% Add ones to the hidden layer in the first column
+a2 = [ones(1, size(z2, 2)); @sigmoid(z2)]; % 26x5000
+% Calculate the output layer
+z3 = Theta2 * a2; % 10x5000 
+a3 = @sigmoid(z3); % 10x5000
+h = a3; % 10x5000
+
+% Now we tranform the y result vector into a matrix where 1s in the
+% columns map to the corresponding values of y
+yMatrix = zeros(num_labels, m);
+
+for i=1:num_labels,
+    yMatrix(i,:) = (y==i); % 10x5000
+end
+
+% Now that we have y as a 10x5000 matrix instead of a 5000x1 vector,
+% we can use it to calculate our cost as compared to h (which is a3)
+
+% Note that for this vectorized implementation, y(i)k is given as
+% yMatrix and h is given as h(thetha)(x(i))k
+
+J = (sum( sum( -1*yMatrix.*log(h) - (1 - yMatrix).*log(1-h) ) ))/m;
+
+% Implementing regularization
+
+% For this we can steal some of the logic from ex2 costFunctionReg.m
+% First, we toss the first columns of each Theta(i) matrix.
+Theta1Reg = Theta1(:,2:size(Theta1,2));
+Theta2Reg = Theta2(:,2:size(Theta2,2));
+
+% Now implement the regularization formula described on page 6 of ex4.
+Reg = (lambda/(2*m)) * (sum(sum( Theta1Reg.^2 )) + sum( sum( Theta2Reg.^2 ) ));
+
+% Now just add the regularization term to the previously calculated J
+J = J + Reg;
 
 
+% Vectorized
+% calculte penalty
+% p = sum(sum(Theta1(:, 2:end).^2, 2))+sum(sum(Theta2(:, 2:end).^2, 2));
+% calculate J
+% J = sum(sum((-Y).*log(h) - (1-Y).*log(1-h), 2))/m + lambda*p/(2*m);
 
+% Part 2: Implement the backpropagation algorithm to compute the gradients
+X = [ones(m,1) X];
 
+for t = 1:m
+  % Step 1 - Feedforward
+  a1 = X(t,:); % 1x401 % Get velues form column m
+  z2 = Theta1 * a1'; % 25x1
+  a2 = [ones(1, size(z2, 2)); @sigmoid(z2)]; % 26x1
+  z3 = Theta2 * a2; % 10x1 
+  a3 = @sigmoid(z3); % 10x1
+  
+  % Step 2 - Calculate d3 from the output layer
+  d3 = a3 - yMatrix(:,t); % Get only the values from column m % 10x1
+  % Step 3 - Calculate d3 for the hidden layer L2
+  % Re-add a bias node for z2
+  z2 = [1; z2]; % 26x1
+  d2 = (Theta2' * d3) .* sigmoidGradient(z2); % 26x1
+  % Step 4 - Strip out bias node from resulting d2
+  d2 = d2(2:end); % 25x1
+  Theta2_grad = (Theta2_grad + d3 * a2'); % 10x26
+  Theta1_grad = (Theta1_grad + d2 * a1); % 25x401
+  
+end
 
+% Step 5 - Calculate the (unregulized) gradients
+% Note that for regularization these will have to removed/commented out.
+%Theta2_grad = Theta2_grad ./ m;
+%Theta1_grad = Theta1_grad ./ m;  
 
+% Part 3: Implement regularization with the cost function and gradients.
 
+% Implement for Theta1 and Theta2 when l = 0
+Theta1_grad(:,1) = Theta1_grad(:,1)./m;
+Theta2_grad(:,1) = Theta2_grad(:,1)./m;
 
+% Implement for Theta1 and Theta 2 when l > 0
+Theta1_grad(:,2:end) = Theta1_grad(:,2:end)./m + ( (lambda/m) * Theta1(:,2:end) );
+Theta2_grad(:,2:end) = Theta2_grad(:,2:end)./m + ( (lambda/m) * Theta2(:,2:end) );
 
+% Vectorized
+% calculate sigmas
+% sigma3 = a3.-Y;
+% sigma2 = (sigma3*Theta2).*sigmoidGradient([ones(size(z2, 1), 1) z2]);
+% sigma2 = sigma2(:, 2:end);
 
+% accumulate gradients
+% delta_1 = (sigma2'*a1);
+% delta_2 = (sigma3'*a2);
 
-
-
-
-
-
-
+% calculate regularized gradient
+% p1 = (lambda/m)*[zeros(size(Theta1, 1), 1) Theta1(:, 2:end)];
+% p2 = (lambda/m)*[zeros(size(Theta2, 1), 1) Theta2(:, 2:end)];
+% Theta1_grad = delta_1./m + p1;
+% Theta2_grad = delta_2./m + p2;
 
 % -------------------------------------------------------------
 
